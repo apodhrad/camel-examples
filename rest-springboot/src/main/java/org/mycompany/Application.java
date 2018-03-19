22bf939eb24fbf96964aaa1388911841c9811476
Andrej Podhradsky <apodhrad@redhat.com>
2018-03-19 11:25:32 +0100

    Add springboot example for rest dsl
    Signed-off-by: Andrej Podhradsky <apodhrad@redhat.com>

---
+++ rest-springboot/src/main/java/org/mycompany/Application.java
@@ -0,0 +1,24 @@
+package org.mycompany;
+
+import org.apache.camel.component.servlet.CamelHttpTransportServlet;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ImportResource;
+
+@SpringBootApplication
+@ImportResource({ "classpath:META-INF/spring/camel-context.xml" })
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	public ServletRegistrationBean camelServletRegistrationBean() {
+		ServletRegistrationBean registration = new ServletRegistrationBean(new CamelHttpTransportServlet(), "/*");
+		registration.setName("CamelServlet");
+		return registration;
+	}
+}
