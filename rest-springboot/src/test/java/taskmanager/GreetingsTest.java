b0f71fa3c8b9f4cee951d964a9758a84e228c567
Andrej Podhradsky <apodhrad@redhat.com>
2018-03-19 15:57:32 +0100

    Use BOMs instead of specific artifact versions
    Signed-off-by: Andrej Podhradsky <apodhrad@redhat.com>

--- rest-springboot/src/test/java/taskmanager/GreetingsTest.java
+++ rest-springboot/src/test/java/taskmanager/GreetingsTest.java
@@ -3,19 +3,22 @@ package taskmanager;
 import static io.restassured.RestAssured.get;
 import static org.hamcrest.Matchers.equalTo;
 
-import org.apache.camel.test.spring.CamelSpringTestSupport;
+import org.apache.camel.test.spring.CamelSpringBootRunner;
 import org.junit.Test;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.junit.runner.RunWith;
+import org.mycompany.Application;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.TestPropertySource;
 
-public class GreetingsTest extends CamelSpringTestSupport {
+@RunWith(CamelSpringBootRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT, classes = Application.class)
+@TestPropertySource(locations = "classpath:application.properties")
+public class GreetingsTest {
 
 	@Test
 	public void testHelloGreeting() throws Exception {
 		get("/greetings/hello/World").then().body(equalTo("Hello World"));
 	}
 
-	@Override
-	protected ClassPathXmlApplicationContext createApplicationContext() {
-		return new ClassPathXmlApplicationContext("META-INF/spring/camel-context.xml");
-	}
 }
